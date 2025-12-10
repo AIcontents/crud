156719d163799002ffed9bd81b0bcb894cc7dd01
dartsol2805 <dartsol2805@gmail.com>
2025-12-10 16:15:24 +0000

    Fix compilation errors

--- src/main/java/com/example/crudapp/MainApp.java
+++ src/main/java/com/example/crudapp/MainApp.java
@@ -1,22 +1,24 @@
-
 package com.example.crudapp;
 
-import com.example.crudapp.dao.Database;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Database.init();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/EntityList.fxml"));
-        primaryStage.setTitle("CRUD App");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        // The Database class is now initialized via a static block.
+        // No need to call a separate init() method.
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("CRUD Application");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
